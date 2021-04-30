919b090d7e7a94f1ad4c60fe50c9b27bd068b40d
Gerardo Ponce <gerardoponcepari@gmail.com>
2021-04-29 21:03:57 -0500

    agregado CRUD para Post v1

--- controllers/postController.js
+++ controllers/postController.js
@@ -10,5 +10,43 @@ module.exports = {
                 console.log(err);
             }
         });
+    },
+    create: function(req,res,next) {
+        let titulo = req.body.titulo;
+        let descripcion = req.body.descripcion;
+        let categoria = req.body.categoria;
+        let fecha = req.body.fecha;
+        let comentarios = {
+            autor: req.body.comentarios.autor,
+            mensaje: req.body.comentarios.mensaje,
+            fecha: req.body.comentarios.fecha,
+        };
+
+        let post = new modelPost({
+            titulo,
+            descripcion,
+            categoria,
+            fecha,
+            comentarios,
+        });
+        post.save().then((data) => {
+            res.send(data);
+        });
+    },
+    update: function(req, res, next) {
+        modelPost.findByIdAndUpdate(req.params.id, req.body, (err, post) => {
+            if (err) {
+                return res.status(500).send({error: 'Ocurrió un problema'})
+            };
+            res.send({success: 'Se actualizó correctamente'});
+        })
+    },
+    delete: function(req, res, next) {
+        modelPost.findByIdAndDelete(req.params.id, (err, post) => {
+            if(err){
+                return res.status(500).send({error: 'Ocurrió un problema'})
+              }
+              res.send({success: 'Se eliminó correctamente'})
+        })
     }
 };
--- routes/post.js
+++ routes/post.js
@@ -6,4 +6,16 @@ router.get('/listar', function(req, res) {
     controller.show(req, res);
 });
 
+router.post('/create', function(req, res) {
+    controller.create(req, res);
+});
+
+router.patch('/update/:id', function(req, res) {
+    controller.update(req, res);
+});
+
+router.delete('/remove/:id', function(req, res) {
+    controller.delete(req, res)
+})
+
 module.exports = router;
